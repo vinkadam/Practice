c88528090c0cec389ba71a7bd78cbe7cd46d2a0b
vinkadam <vinay001.kadam@gmail.com>
2017-03-03 11:59:03 +0530

    Adapter design pattern

---
+++ DesignPatternExamples/src/com/example/structural/adapter/AdapterPatternTest.java
@@ -0,0 +1,35 @@
+package com.example.structural.adapter;
+
+public class AdapterPatternTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Class adapter example");
+		System.out.println("***********");
+		testClassAdapter();
+		
+		System.out.println("\nObject adapter example");
+		System.out.println("***********");
+		testObjectAdapter();
+	}
+
+	private static void testObjectAdapter() {
+		SocketAdapter sockAdapter = new SocketObjectAdapterImpl();
+		Volt v3 = sockAdapter.get3Volt();
+		Volt v12 = sockAdapter.get12Volt();
+		Volt v120 = sockAdapter.get120Volt();
+		System.out.println("v3 volts using Object Adapter="+v3.getVolts());
+		System.out.println("v12 volts using Object Adapter="+v12.getVolts());
+		System.out.println("v120 volts using Object Adapter="+v120.getVolts());
+	}
+
+	private static void testClassAdapter() {
+		SocketAdapter sockAdapter = new SocketClassAdapterImpl();
+		Volt v3 = sockAdapter.get3Volt();
+		Volt v12 = sockAdapter.get12Volt();
+		Volt v120 = sockAdapter.get120Volt();
+		System.out.println("v3 volts using Class Adapter="+v3.getVolts());
+		System.out.println("v12 volts using Class Adapter="+v12.getVolts());
+		System.out.println("v120 volts using Class Adapter="+v120.getVolts());
+	}
+}
---
+++ DesignPatternExamples/src/com/example/structural/adapter/SocketAdapter.java
@@ -0,0 +1,11 @@
+package com.example.structural.adapter;
+
+// This can be a abstract class also
+public interface SocketAdapter {
+
+	public Volt get120Volt();
+		
+	public Volt get12Volt();
+	
+	public Volt get3Volt();
+}
